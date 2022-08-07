c79a7d98d438206ee619d0b625bb14fdae46418f
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2022-08-08 01:17:24 +0530

    added head for HTML meta and link tags

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -18,5 +18,19 @@ export default {
     footer: {
       message: 'Released under the Open Sourse GPL 3.0 License',
     },
-  }
+  },
+
+  // HTML head tags
+  head: [
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'icon', href: '/favicon.png', type: 'image/png' }],
+    ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
+    ['meta', { name: 'author', content: 'Adarsh Madrecha' }],
+    ['meta', { property: 'og:title', content: 'Due Date' }],
+    ['meta', { property: 'og:image', content: 'https://date.jamku.app/favicon.png' }],
+    ['meta', { property: 'og:description', content: 'Database of Due Date for Indian Laws' }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:creator', content: '@adarshmadrecha' }],
+    ['meta', { name: 'twitter:image', content: 'https://vueuse.org/favicon.png' }],
+  ],
 }
