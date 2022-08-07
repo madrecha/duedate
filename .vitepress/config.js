18cf73b5bf3c037eb1e91bbf28c4e3c077283d5b
Adarsh Madrecha <adarshmadrecha@yahoo.com>
2022-08-08 00:25:12 +0530

    show last updated date

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,6 +1,7 @@
 export default {
   title: 'Due Date',
   description: 'Indian Compliance Due Dates as per all laws.',
+  lastUpdated: true,
   themeConfig: {
     siteTitle: 'Due Date',
     logo: '/favicon.svg',
